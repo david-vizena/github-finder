c4400d4a60682c0f3e9e3960031cbcfd62a33262
David Vizena <davidvizena@gmail.com>
2022-10-28 13:19:19 -0500

    project complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,9 @@ function About() {
 			<h1 className='text-6xl mb-4'>Github Finder</h1>
 			<p className='mb-4 text-2xl font-light'>
 				A React app to search GitHub profiles and see profile details. This
-				project uses Tailwind CSS with the DaisyUI addon.
+				project was built in React, and uses Tailwind CSS with the DaisyUI
+				addon. It uses Actions, Context and Reducer functionality from React. It
+				has been deployed to production by using Vercel.
 			</p>
 			<p className='text-lg text-gray-400'>
 				Version <span className='text-white'>1.0.0</span>
